ad59f50a0ce8d7c9eb83bb4102b5d9c32a0aaa77
leonardobagi <65085761+leonardobagi@users.noreply.github.com>
2021-12-04 19:04:28 -0400

    Add files via upload

---
+++ promocodesObj.js
@@ -0,0 +1,70 @@
+var promocodes = [
+    {
+        nome: "The Bird Says___",
+        categoria: "Item de ombro",
+        codigo: "TWEETROBLOX",
+        lancamento: "29/05/2015",
+        expiracao: "Nunca",
+        link: "https://www.roblox.com/catalog/253151806/The-Bird-Says"
+    },
+    {
+        nome: "Spider Cola",
+        categoria: "Item de ombro",
+        codigo: "SPIDERCOLA",
+        lancamento: "10/05/2019",
+        expiracao: "Nunca",
+        link: "https://www.roblox.com/catalog/3164811019/Spider-Cola",
+    },
+    {
+        nome: "Dev Deck",
+        categoria: "Item de costas",
+        codigo: "ROBLOXEDU2021",
+        lancamento: "22/04/2021",
+        expiracao: "???",
+        link: "https://www.roblox.com/catalog/6594369919/Dev-Deck"
+    },
+    {
+        nome: "King Tab",
+        categoria: "Item de cabeça (cabeça)",
+        codigo: "SMYTHSCAT2021",
+        parceria: ["Smyths Toys", "https://www.google.com/search?q=Smyths+Toys"],
+        lancamento: "17/09/2021",
+        expiracao: "???",
+        link: "https://www.roblox.com/catalog/6763676405/King-Tab"
+    },
+    {
+        nome: "Snow Friend",
+        categoria: "Item de ombro",
+        codigo: "AMAZONFRIEND2021",
+        parceria: ["Amazon", "https://www.google.com/search?q=Amazon"],
+        lancamento: "07/10/2021",
+        expiracao: "31/12/2021",
+        link: "https://www.roblox.com/catalog/6833450795/Snow-Friend"
+    },
+    {
+        nome: "Peppermint Hat",
+        categoria: "Item de cabeça (chapéu)",
+        codigo: "TARGETMINTHAT2021",
+        parceria: ["Target", "https://www.google.com/search?q=Target"],
+        lancamento: "18/10/2021",
+        expiracao: "???",
+        link: "https://www.roblox.com/catalog/6833438800/Peppermint-Hat"
+    },
+    {
+        nome: "Crown of Electrifying Guitars",
+        categoria: "Item de cabeça (coroa)",
+        codigo: "ROSSMANNCROWN2021",
+        lancamento: "23/11/2021",
+        expiracao: "???",
+        link: "https://www.roblox.com/catalog/7084601073/Crown-of-Electrifying-Guitars"
+    },
+    {
+        nome: "White Flamingo Fedora",
+        categoria: "Item de cabeça (chapéu/fedora)",
+        codigo: "MERCADOLIBREFEDORA2021",
+        parceria: ["Mercado Livre", "https://www.google.com/search?q=Mercado+Livre"],
+        lancamento: "26/11/2021",
+        expiracao: "???",
+        link: "https://www.roblox.com/catalog/6971068380/White-Flamingo-Fedora"
+    },
+]
